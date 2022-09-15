184c6da866efd587f2e00f99b0a6b539ac753aaf
gideonthomasd <56291811+gideonthomasd@users.noreply.github.com>
2022-09-15 22:00:15 +0100

    Create 240Nvidia-Steam.sh

---
+++ 240Nvidia-Steam.sh
@@ -0,0 +1,11 @@
+#In the scenario that it has not, select the following packages to install, assuming you are using an NVIDIA GPU from the Geforce 600 series and newer:
+
+#nvidia-computeG05 - NVIDIA driver for computing with GPGPU
+#nvidia-gfxG05-kmp-default - NVIDIA graphics driver kernel module for GeForce 600 series and newer
+#nvidia-glG05 - NVIDIA OpenGL libraries for OpenGL acceleration
+#x11-video-nvidiaG05 - NVIDIA graphics driver for GeForce 600 series and newer
+#kernel-firmware-nvidia - Kernel firmware files for Nvidia Tegra and graphics drivers
+#Uninstall the xf86-video-nouveau package, and then reboot your machine to complete the process.
+
+
+#Install Steam
